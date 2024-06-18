25333c5ef636e5d80c6dcacecaf04600b1d30499
Edward xc <3313338649@qq.com>
2024-06-18 09:47:53 +0800

    update init.sql and add function ，添加模拟amin注册登录功能

--- Function.sql
+++ Function.sql
@@ -1,8 +1,8 @@
--- 注册和登录模块功能DELIMITER //
+-- 注册和登录模块功能
 -- 用户功能模块
 -- 注册用户的存储过程
 CREATE PROCEDURE register_user (
-    IN p_userid INT, -- 这里添加了逗号分隔符
+    IN p_userid INT,
     IN p_username VARCHAR(50),
     IN p_password VARCHAR(255),
     IN p_email VARCHAR(100),
@@ -42,4 +42,54 @@ CALL register_user(51, 'john_doe', 'hashed_password_value', 'john_doe@example.co
 CALL login_user('john_doe', 'input_password_value');
 -- 用户登录成功
 CALL login_user('john_doe', 'hashed_password_value');
+
+-- 管理员注册登录
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE login_admin (
+    IN p_userid INT,
+    IN p_password VARCHAR(255)
+)
+BEGIN
+    DECLARE hashed_password VARCHAR(255);
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND
+    BEGIN
+        SELECT '管理员ID不存在！' AS result;
+    END;
+
+    SELECT password INTO hashed_password
+    FROM admins
+    WHERE admin_id = p_userid;
+
+    IF hashed_password = p_password THEN
+        SELECT '管理员登录成功！' AS result;
+    ELSE
+        SELECT '管理员登录失败，请检查用户名和密码。' AS result;
+    END IF;
+END//
+
+-- 注册管理员存储过程
+CREATE PROCEDURE register_admin (
+    IN p_userid INT,
+    IN p_username VARCHAR(255),
+    IN p_password VARCHAR(255)
+)
+BEGIN
+    INSERT INTO admins (admin_id, username, password)
+    VALUES (p_userid, p_username, p_password);
+END//
+
+DELIMITER ;
+
+-- 测试样例
+-- 注册管理员
+CALL register_admin(52,'admin_user', 'admin_user');
+-- 登录管理员
+-- 失败
+CALL login_admin(51,'admin_user');
+-- 成功
+CALL login_admin(52,'admin_user');
 
